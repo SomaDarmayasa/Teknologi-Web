7a183010f8567c858c77d7bd1b94e0fcd8cb5a6c
SomaDarmayasa <somadarmayasa08@gmail.com>
2021-02-25 21:15:17 +0700

    project 1

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>OBYEK WISATA DI UBUD</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-left "style = "backgroud-color = aliceblue; ">
+  <h1 style ="margin :20px; ;">Obyek Wisata Di Ubud <h1>
+</div>
+<div class="container" style="float : left;">
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#">Home</a></li>
+    <li><a href="#">Artikel</a></li>
+    <li><a href="#">Penginapan</a></li>
+    <li><a href="#">Kuliner</a></li>
+  </ul>
+  <br>
+  </div>
+ 
+ <div  class="post-info" style="float : left;">
+Di  Posting  Oleh  <b>Admin</b>
+</div>
+
+  
+  
+<div class="container" style=" background-color: cornsilk;float : left; width : 700px;">
+<h1> Sekilas Tentang Ubud </h1>
+<img src="https://www.nativeindonesia.com/wp-content/uploads/2019/10/tempat-wisata-Ubud.jpg" class="img-rounded" alt="Cinque Terre" width="650" height="400"> 
+<p1>Sebagai kawasan pariwisata Ubud memiliki daya tarik yang berbeda dengan kawasan Badung. Jika Badung daya tarik utama ada pada pantai, sebaliknya kawasan Ubud daya tarik utama terdapat pada seni, budaya, alam pedesaan, serta aktivitas wisata.
+
+Walaupun setiap wilayah di Bali memiliki banyak seni, namun bagi wisatawan kawasan ini adalah pusat dari seni serta budaya Bali. Hasil seni yang terkenal seperti seni tari, seni lukis, seni patung dan seni ukir. Untuk wisata budaya, banyak terdapat pura Hindu dengan arsitektur yang unik.Untuk wisata alam, daya tarik utama terdapat pada hutan hujan tropis yang dihuni kera ekor panjang, serta areal sawah terasering. Wisata petualangan juga menjadi daya tarik yang membuat wisatawan tertarik liburan ke kawasan ini. Aktivitas wisata petulangan seperti rafting disungai Ayung, naik ATV di area pedesaan, serta naik gajah di desa Taro.
+
+Ubud yang terletak di sebelah utara kota Denpasar, dengan waktu tempuh kurang lebih 2 jam dari Bandara Internasional I Gusti Ngurah Rai ini menawarkan pesona tersendiri dari eksotisme lain pulau Bali.</p1>
+</div>
+   
+   <div  class="col-lg-4  "  style="background-color:  aliceblue ; float :right;">
+		<div  class="widget  ">
+		<h2>Trending</h2>
+		<ul  class="menusidebar  ">
+		<li><a  href="  ">Obyek Wisata Candi Borobudur</a></li>
+		<li><a  href="  ">Obyek Wisata Malioboro</a></li>
+		<li><a  href="">Pulau Komodo</a></li>
+		<li><a  href="">Raja Ampat</a></li>
+		<li><a  href="">Obyek Wisata Gunung Bromo</a></li>
+        <li><a  href="">Taman Nasional Wakatobi</a></li>
+		<li><a  href="">Kawah Ijen</a></li>
+		<li><a  href="">Taman Nasional Bunaken</a></li>
+				</ul>
+				</div>
+			</div>
+		</div>
+        
+        <div class="container" style="float:right; width : 300px">
+        <h2> Leave a Reply </h2>
+  <form>
+    <div class="form-group">
+      <label for="usr">E-Mail Name:</label>
+      <input type="text" class="form-control" id="usr">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd">
+    </div>
+<div class="form-group">
+      <label for="comment">Comment:</label>
+      <textarea class="form-control" rows="5" id="comment"></textarea>
+    </div>
+  </form>
+</div>
+
+
+	<div class="container" style=" background-color: cornsilk;float : left; width : 700px;">
+		<h1> 1. Monkey Forest </h1>
+		<img src="media/monkey-forest.jpg" class="img-rounded" alt="Cinque Terre" width="650" height="400"> 
+
+<p1> Monkey Forest Ubud ini juga dikenal dengan nama Mandala Wisata Wenara Wana. Seperti namanya, tempat ini merupakan habitat yang secara khusus dibuat untuk tempat tinggal para monyet. Bahkan total terdapat sebanyak 340 ekor monyet yang tinggal di hutan ini.
+
+Terdapat sekitar empat kelompok monyet yang berdomisili di tempat wisata Ubud ini. Monyet yang tinggal di sini termasuk adalah monyet ekor panjang yang mempunyai nama latin Macaca fascicularis. Tak hanya itu, Hutan Monyet Ubud ini juga merupakan rumah untuk 115 spesies pohon.
+
+Di sini juga terdapat pura yang disebut dengan nama Candi Pura Dalem Agung Padangtegal atau disebut juga Sacred Monkey Forest Sanctuary. Masyarakat setempat pun kerap melangsungkan upacara khusus di pura yang dilakukan sebagai bentuk penghormatan kepada para hewan. Perayaan itu disebut dengan nama Tumpek Kandang .
+
+Saat berkunjung ke tempat wisata ubud ini, taruhlah barang berharga di dalam tas. Seperti anting-anting, kacamata, ataupun handphone. Jika ingin foto-foto, selalu pegang erat kamera. Barang-barang tersebut akan menarik minat dari kera-kera untuk mengambil.
+
+Alamat:  Jl. Monkey Forest, Ubud, Kecamatan Ubud, Kabupaten Gianyar,
+Tiket: 40.000
+Jam Buka: 08.30-17.30 </p1>
+
+</div>
+
+<div class="container" style="background-color: cornsilk; float : left; width : 700px;">
+		<h1> 2. Bukit Campuhan </h1>
+		<img src="media/BUkit-campuhan.jpg" class="img-rounded" alt="Cinque Terre" width="650" height="400"> 
+        <p1>	Pemandangan Bukit Campuhan Ubud kini menjadi destinasi wisata yang populer di kalangan para wisatawan. Lokasinya yang tersembunyi, membuat tempat ini jadi lokasi favorit, terutama karena bisa sejenak merasakan kesunyian, jauh dari ingar bingar suasana perkotaan.
+
+Tempat wisata Ubud satu ini tidak hanya menawarkan indahnya pemandangan alam. Refresing di bukit ini juga mampu mengusir segala kepenatan. Pemandangan asri di Bukit Campuhan Ubud semakin nampak asyik dengan jalur trekking yang tertata rapi sepanjang 2 kilometer.
+
+Di kawasan Bukit Campuhan Ubud terdapat sebuah pura yang bernama Pura Gunung Lebah. Bukit tempat wisata Ubud ini diapit oleh dua anak sungai sehingga terbentuk lembah-lembah hijau yang rimbun oleh pepohonan tropis.
+
+Rumput-rumput ilalang juga tumbuh dengan sangat baik sehingga sepanjang mata memandang hanya tampak hijaunya alam yang menyegarkan.Jika berencana datang ke Bukit Campuhan, baiknya datang pada pagi hari atau sore hari. Karena ketika pagi udara masih sangat segar dengan matahari yang hangat. Jangan datang ketika siang jika tidak ingin terpapar panasnya matahari karena tidak adanya tempat berteduh.
+
+Alamat: Jalan Bangkiang Sidem, Kelusa, Payangan, Ubud, Kabupaten Gianyar
+Tiket: Gratis
+Jam Buka: 24 Jam </p1>
+</div>
+</div>
+<div class="container" style=" background-color: cornsilk; float : left; width : 700px;">
+		<h1> 3. Pasar Kesenian Ubud </h1>
+		<img src="media/PAsar-seni-ubud.jpg" class="img-rounded" alt="Cinque Terre" width="650" height="400"> 
+        <p1>Barang dagangan yang ada di pasar tradisional Ubud tergolong lumayan berkualitas dengan harga yang relatif murah. Tapi untuk mendapatkan harga murah, pembeli harus pandai dalam menawar. Sebagian besar pedagang mengambil barang dagangan langsung dari pengerajin rumahan. Yang membuat harga barang menjadi lebih murah.
+
+Ada dua wilayah utama di dalam pasar seni tempat wisata Ubud ini. Wilayah barat dan wilayah timur. Wilayah barat pasar, lebih banyak menjual barang dagangan berupa hasil kerajinan seni. Wilayah timur lebih banyak menjual kebutuhan bahan pokok sehari-hari seperti gula, beras dan sembako yang lainnya.
+
+Waktu terbaik mengunjungi pasar tempat wisata Ubud ini adalah di pagi hari. Karena ada kepercayaan orang Bali untuk memperlancar jualan selama satu hari, harus berhasil menjual barang dagangan di pagi hari, walaupun untungnya tipis.
+
+Jadi jika anda ingin berbelanja di Ubud art market dan ingin mendapatkan peluang menawar dengan harga murah, maka sebaiknya anda berkunjung di pagi hari. Selain dapat menawar dengan harga murah saat berkunjung di pagi hari, pasar seni tradisional Ubud juga tidak terlalu ramai pada pagi hari.
+
+Alamat: Jalan Raya Ubud No.35, Ubud, Gianyar, Bali.
+Tiket: Gratis
+Jam Buka: 04.00-18.00 </p1>
+	</div>
+</div>
+</body>
+</html>
